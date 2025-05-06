227b7e247598baae89189367e9eaaeeb9a18ea08
maeilert <maeilert@stud.ntnu.no>
2025-05-06 14:19:26 +0200

    cross platform integration mac/linux path

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import MySQLdb
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': 'my.cnf'
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf')
         }
     }
 }

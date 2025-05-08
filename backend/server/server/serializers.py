94bbdaadc350719fb044e586e1c95830046db840
maeilert <maeilert@stud.ntnu.no>
2025-05-08 13:55:20 +0200

    implemented combining of serializers

--- backend/server/server/serializers.py
+++ backend/server/server/serializers.py
@@ -1,14 +1,20 @@
 # serializers.py
 from rest_framework import serializers
-from .models import Product, Brand
+from .models import Product, Brand, Category
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = '__all__'
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
 class ProductSerializer(serializers.ModelSerializer):
-    brand_details = BrandSerializer(source='brand', read_only=True)
+    brand = BrandSerializer(read_only=True)          # Override the brand field
+    categoryid = CategorySerializer(read_only=True)  # Override the category field
     
     class Meta:
         model = Product
